aaf107c9419407ce76dde9bd8ad6027c61bb81cc
IABorges <151062175+IABorges@users.noreply.github.com>
2024-02-23 23:12:18 +0000

    Primeiro codigo em C, simplesmente recebe dois numeros e soma

---
+++ teste.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+
+int soma(int a, int b){
+   return a + b;
+}
+
+
+
+
+int main(){
+
+   int a  ;
+   int b ;
+   printf("Eai\n");
+   printf("Digita ai malandro\n");
+   scanf("%d",&a);
+
+   printf("Digita outro numero comedia\n");
+   scanf("%d",&b);
+   int z = soma(a,b);
+   printf("%d * %d = %d", a,b,z);
+
+}
+
+
+
+
+
